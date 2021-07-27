11e78f62e3402f13da7cc07cd58090550af2e9fa
Ashley Chung <ahchung722@gmail.com>
2021-07-27 06:25:36 -0700

    comments on demo.c

--- src/client/demo.c
+++ src/client/demo.c
@@ -9,17 +9,20 @@ int main ()
 {
   #ifdef OPENFile
   FILE * pFile;
+  //trying to open the file
   pFile = fopen ("OPENFILE,"rb");
+  //if the file is empty, then write error
   if (pFile == NULL)
   {
     my_perror("The following error occurred");
     my_printf( "Value of errno: %d\n", errno );
   }
+  //if it can open, we are going to clise it 
   else {
     fclose (pFile);
   }
   #endif
-  
+
   return 0;
-  
+
 }
