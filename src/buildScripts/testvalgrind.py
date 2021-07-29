1e56baf3c373b97ccdaecdd8213ec71f64239622
hendersonmach <m233714@usna.edu>
2021-07-29 14:45:53 -0400

    Revert "updated testing program"
    This reverts commit 0da30d0d6ddbf8fb296efd52233feae8a9fe2126.

--- src/buildScripts/testvalgrind.py
+++ src/buildScripts/testvalgrind.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-cmd = ["valgrind", "--leak-check=full", "python3", "compiler.py"]
+cmd = ["valgrind", "--leak-check=full", "./compiler.py"]
 subprocess.run(cmd)
 
 #valgrind --leak-check=full ./compiler.py
