146c89f6a05d8dc0d843cc6cc0e30be4c56e05a1
hendersonmach <m233714@usna.edu>
2021-08-16 15:28:00 -0400

    changed to handle executables as well as other files. to use, just make sure the headers are in somewhere in the code, as well as the static size_t portion, then just copy paste the whole if statement into where all the other commands are

--- src/buildScripts/downloadurl.c
+++ src/buildScripts/downloadurl.c
@@ -10,49 +10,37 @@ static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
   return written;
 }
 
-int main(int argc, char *argv[])
+int main()
 {
-  CURL *curl_handle;
-  static const char *pagefilename = "image.jpg";
-  FILE *pagefile;
 
-  if(argc < 2) {
-    printf("Usage: %s <URL>\n", argv[0]);
-    return 1;
-  }
+  CURL *curl_handle;
+  static const char *pagefilename = "downloadedfile";
+  FILE *pagefile;
+  char urlname[99] = "butts";
+  scanf("%s", urlname);
 
   curl_global_init(CURL_GLOBAL_ALL);
 
-  /* init the curl session */
   curl_handle = curl_easy_init();
 
-  /* set URL to get here */
-  curl_easy_setopt(curl_handle, CURLOPT_URL, argv[1]);
+  curl_easy_setopt(curl_handle, CURLOPT_URL, urlname);
 
-  /* Switch on full protocol/debug output while testing */
   curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
 
-  /* disable progress meter, set to 0L to enable it */
   curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
 
-  /* send all data to this function  */
   curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
 
-  /* open the file */
   pagefile = fopen(pagefilename, "wb");
   if(pagefile) {
 
-    /* write the page body to this file handle */
     curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
 
-    /* get it! */
     curl_easy_perform(curl_handle);
 
-    /* close the header file */
     fclose(pagefile);
   }
 
-  /* cleanup curl stuff */
   curl_easy_cleanup(curl_handle);
 
   curl_global_cleanup();
